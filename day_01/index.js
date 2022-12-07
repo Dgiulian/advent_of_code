6c07408fc4f6aaeca7df14997adaaab508c996cb
Diego Giuliani <giuliani.diego>
2022-12-07 00:22:26 -0300

    Day 1 Input

---
+++ day_01/index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+
+fs.readFile;
+
+function getDeerCaloriesSum(deers) {
+  return deers.map((deer) =>
+    deer
+      .filter((food) => food)
+      .map((food) => parseInt(food, 10))
+      .reduce((sum, f) => sum + f, 0)
+  );
+}
+
+async function partTwo(fileName) {
+  try {
+    const fileBuffer = await fs.promises.readFile(fileName);
+    const file = fileBuffer.toString();
+    const deers = file.split("\n\n").map((line) => line.split("\n"));
+
+    const result = getDeerCaloriesSum(deers);
+
+    result.sort((a, b) => (parseInt(a) > parseInt(b) ? -1 : 1));
+
+    const sumTop3 = result.slice(0, 3).reduce((sum, cal) => sum + cal, 0);
+    console.log(sumTop3);
+  } catch (error) {
+    console.error("There was an error reading the file", error);
+  }
+}
+
+async function partOne(fileName) {
+  try {
+    const fileBuffer = await fs.promises.readFile(fileName);
+    const file = fileBuffer.toString();
+    const deers = file.split("\n\n").map((line) => line.split("\n"));
+
+    const result = getDeerCaloriesSum(deers);
+
+    result.sort((a, b) => (parseInt(a) > parseInt(b) ? -1 : 1));
+
+    console.log(result[0]);
+  } catch (error) {
+    console.error("There was an error reading the file", error);
+  }
+}
+
+partOne("input_01.txt");
+partTwo("input_01.txt");
