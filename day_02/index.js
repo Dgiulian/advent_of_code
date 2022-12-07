2b6efed812c802ae79ba046e574696a33dc8d7be
Diego Giuliani <giuliani.diego>
2022-12-07 01:13:23 -0300

    Finish Part 2 of Day 2

--- day_02/index.js
+++ day_02/index.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 
-// A, X -> Rock -> 1
-// B, Y -> Paper -> 2
-// C, Z -> Scissor -> 3
-
 /**
  *
  * Score
@@ -12,7 +8,8 @@ const fs = require("fs");
  * Win -> 6
  *
  */
-const shape_values = { X: 1, Y: 2, Z: 3 };
+const shape_values = { X: 1, Y: 2, Z: 3, A: 1, B: 2, C: 3 };
+
 const scores = {
   AX: 3,
   AY: 6,
@@ -23,9 +20,23 @@ const scores = {
   CX: 6,
   CY: 0,
   CZ: 3,
+
+  AA: 3,
+  AB: 6,
+  AC: 0,
+
+  BA: 0,
+  BB: 3,
+  BC: 6,
+  CA: 6,
+  CB: 0,
+  CC: 3,
 };
 
 async function partOne(fileName) {
+  // A, X -> Rock -> 1
+  // B, Y -> Paper -> 2
+  // C, Z -> Scissor -> 3
   const fileBuffer = await fs.promises.readFile(fileName);
   const file = fileBuffer.toString();
   const plays = file
@@ -50,4 +61,68 @@ async function partOne(fileName) {
   return totalScore;
 }
 
-partOne("input_02.txt");
+async function partTwo(fileName) {
+  /*
+  X -> Loose
+  Y -> Draw
+  Z -> Win
+  A, X -> Rock -> 1
+  B, Y -> Paper -> 2
+  C, Z -> Scissor -> 3
+  */
+  const complement_shapes = {
+    A: {
+      X: "C",
+      Y: "A",
+      Z: "B",
+    },
+    B: {
+      X: "A",
+      Y: "B",
+      Z: "C",
+    },
+    C: {
+      X: "B",
+      Y: "C",
+      Z: "A",
+    },
+  };
+
+  const fileBuffer = await fs.promises.readFile(fileName);
+  const file = fileBuffer.toString();
+  const plays = file
+    .split("\n")
+    .filter((line) => line)
+    .map((line) => line.split(" "));
+
+  const rounds = [];
+
+  for (let i = 0; i < plays.length; i += 3) {
+    rounds.push([...plays.slice(i, i + 3)]);
+  }
+  let totalScore = 0;
+  for (let round of rounds) {
+    const sum = round.reduce((acum, play) => {
+      console.log({
+        play,
+        selectedShape: complement_shapes[play[0]][play[1]],
+        p: [play[0], complement_shapes[play[0]][play[1]]].join(""),
+        score:
+          scores[[play[0], complement_shapes[play[0]][play[1]]].join("")] +
+          shape_values[complement_shapes[play[0]][play[1]]],
+      });
+      return (
+        acum +
+        scores[[play[0], complement_shapes[play[0]][play[1]]].join("")] +
+        shape_values[complement_shapes[play[0]][play[1]]]
+      );
+    }, 0);
+
+    totalScore += sum;
+  }
+  console.log(totalScore);
+  return totalScore;
+}
+
+// partOne("input_02.txt");
+partTwo("input_02.txt");
