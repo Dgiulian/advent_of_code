d84de9dcb021ef02964c1021860898640ae82ce3
Diego Giuliani <giuliani.diego>
2022-12-09 14:21:33 -0300

    Optimize Day 09 Solution

--- day_09/index.js
+++ day_09/index.js
@@ -4,14 +4,7 @@ const fs = require("fs");
 const visitedPositions = new Set();
 
 /* @type string[][] */
-const state = [
-  ["", "", "", "", "", ""],
-  ["", "", "", "", "", ""],
-  ["", "", "", "", "", ""],
-  ["", "", "", "", "", ""],
-  ["", "", "", "", "", ""],
-];
-
+const state = [5, 6];
 const start = [0, 0];
 const ROW = 0;
 const COL = 1;
@@ -30,14 +23,6 @@ async function partOne(fileName) {
     .map((line) => line.split(" "))
     .map((line) => [line[0], parseInt(line[1])]);
 
-  /*  positions.head[ROW] = 2;
-  positions.head[COL] = 3;
-  positions.tail[ROW] = 3;
-  positions.tail[COL] = 1;
-  debugState();
-  console.log("Update Tail");
-  updateTail();
-*/
   for (let movement of movements) {
     executeMovement(movement);
   }
@@ -97,7 +82,6 @@ function updateTail() {
       tail[0] -= 1;
     }
   } else if (head[COL] + 1 === tail[COL]) {
-    /* TODO: Diagonals???? */
     if (head[ROW] + 2 === tail[ROW]) {
       tail[ROW] -= 1;
       tail[COL] -= 1;
@@ -142,20 +126,20 @@ function isEndPoint(
   return positions[end][0] === i && positions[end][1] === j;
 }
 function debugState() {
-  for (let i = 0; i < state.length; i++) {
-    let line = "";
+  for (let i = 0; i < state[0]; i++) {
+    let line = [];
 
-    line = state[i].map((pos, j) => {
+    for (let j = 0; j < state[1]; j++) {
       if (isEndPoint("head", i, j)) {
-        return "H";
+        line.push("H");
       } else if (isEndPoint("tail", i, j)) {
-        return "T";
-      } else return ".";
-    });
+        line.push("T");
+      } else line.push(".");
+    }
 
     console.log([...line].join(""));
   }
   console.log("    ");
 }
 
-partOne("input_09.txt");
+partOne("input_09_small.txt");
