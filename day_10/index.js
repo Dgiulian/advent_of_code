692716735d45d8f69122e9e860edb162218fdaa7
Diego Giuliani <giuliani.diego>
2022-12-10 19:26:24 -0300

    Finish Day 10 part 2

--- day_10/index.js
+++ day_10/index.js
@@ -42,5 +42,57 @@ async function partOne(fileName) {
   console.log(signalStrength);
 }
 
+function drawPixel(cycleCount, X) {
+  let row = Math.floor(cycleCount / 40);
+  let col = cycleCount % 40;
+
+  const sprite = X - 1;
+  if (col >= X - 1 && col <= X + 1) {
+    CRT[row][col] = "#";
+  }
+  console.log({ cycleCount, row, col });
+}
+
+let CRT = Array.from({ length: 6 }).map(() =>
+  Array.from({ length: 40 }).fill(" ")
+);
+
+async function partTwo(fileName) {
+  const instructions = await getInput(fileName);
+  const checks = [20, 60, 100, 140, 180, 220];
+  let X = 1;
+  let cycleCount = 0;
+  let signalStrength = 0;
+
+  for (let instruction of instructions) {
+    if (instruction === "noop") {
+      drawPixel(cycleCount, X);
+      cycleCount += 1;
+      if (checks.includes(cycleCount)) {
+        // console.log({ cycleCount, X, signal: X * cycleCount });
+        signalStrength += X * cycleCount;
+      }
+    } else {
+      const [, V] = instruction.split(" ");
+      for (let i = 0; i < 2; i++) {
+        drawPixel(cycleCount, X);
+        cycleCount += 1;
+        if (checks.includes(cycleCount)) {
+          // console.log({ cycleCount, X, signal: X * cycleCount });
+          signalStrength += X * cycleCount;
+        }
+        if (i == 1) {
+          X += Number(V);
+        }
+      }
+    }
+  }
+  console.log(signalStrength);
+  for (let i = 0; i < CRT.length; i++) {
+    console.log(CRT[i].join(""));
+  }
+}
+
 // partOne("input_10_small.txt");
-partOne("input_10.txt");
+// partOne("input_10.txt");
+partTwo("input_10.txt");
