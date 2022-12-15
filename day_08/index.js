048f39c4fc61bb41e744ef35e5b74e73fff737c1
Diego Giuliani <giuliani.diego>
2022-12-15 08:32:23 -0300

    Finish Part 2 of day 8

--- day_08/index.js
+++ day_08/index.js
@@ -80,8 +80,90 @@ function isVisible(field, i, j) {
 
 async function partTwo(fileName) {
   /** @type string */
-  const [input] = await getInput(fileName);
+  const input = await getInput(fileName);
+  const field = parseField(input);
+  let visibleTreesCount = (field.length - 2) * 2 + field[0].length * 2;
+  console.log(visibleTreesCount);
+
+  const scenicScores = [];
+  let i = 3;
+  let j = 2;
+  // for (let i = 1; i < field.length - 1; i++) {
+  scenicScores[i] = [];
+  // for (let j = 1; j < field.length - 1; j++) {
+  const filteredCandidates = [];
+  const TREE = field[i][j];
+
+  console.log("Tree: ", TREE);
+
+  let CANDIDATES = [];
+  let minHeight = TREE;
+  for (let r = i; r >= 0; r--) {
+    const CANDIDATE = field[r][j];
+    if (CANDIDATE > minHeight) {
+      CANDIDATES.push(CANDIDATE);
+      break;
+    }
+    minHeight = Math.min(minHeight, CANDIDATE);
+    CANDIDATES.push(CANDIDATE);
+  }
+
+  console.log("Top", CANDIDATES);
+
+  filteredCandidates.push(CANDIDATES.length);
+  CANDIDATES = [];
+  for (let r = i + 1; r < field.length; r++) {
+    const CANDIDATE = field[r][j];
+
+    if (CANDIDATE > minHeight) {
+      CANDIDATES.push(CANDIDATE);
+      break;
+    }
+    minHeight = Math.min(minHeight, CANDIDATE);
+
+    CANDIDATES.push(CANDIDATE);
+  }
+  console.log("Bottom", CANDIDATES);
+  filteredCandidates.push(CANDIDATES.length);
+
+  CANDIDATES = [];
+  for (let c = j; c >= 0; c--) {
+    const CANDIDATE = field[i][c];
+    if (CANDIDATE > minHeight) {
+      CANDIDATES.push(CANDIDATE);
+      break;
+    }
+    minHeight = Math.min(minHeight, CANDIDATE);
+    CANDIDATES.push(CANDIDATE);
+  }
+  console.log("Left", CANDIDATES);
+  filteredCandidates.push(CANDIDATES.length);
+
+  CANDIDATES = [];
+  for (let c = j + 1; c < field[0].length; c++) {
+    const CANDIDATE = field[i][c];
+    if (CANDIDATE > minHeight) {
+      CANDIDATES.push(CANDIDATE);
+      break;
+    }
+    minHeight = Math.min(minHeight, CANDIDATE);
+    CANDIDATES.push(CANDIDATE);
+  }
+  console.log("Right", CANDIDATES);
+
+  filteredCandidates.push(CANDIDATES.length);
+  console.log(filteredCandidates);
+  const scenicScore = filteredCandidates.reduce((acc, x) => acc * x);
+  scenicScores[i][j] = scenicScore;
+  //   }
+  // }
+
+  for (let i = 0; i < field.length - 1; i++) {
+    console.log(scenicScores[i]);
+  }
 }
 
 // partOne("input_08_small.txt");
-partOne("input_08.txt");
+// partOne("input_08.txt");
+
+partTwo("input_08_small.txt");
